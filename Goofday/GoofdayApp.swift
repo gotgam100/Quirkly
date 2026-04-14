6585d74caf98acd4e3871de2c61fcf80cb510a9c
gotgam100 <qbeck104@gmail.com>
2026-04-14 16:45:36 +0900

    Initial Commit

---
+++ Goofday/GoofdayApp.swift
@@ -0,0 +1,32 @@
+//
+//  GoofdayApp.swift
+//  Goofday
+//
+//  Created by BAEKMAC on 4/14/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct GoofdayApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Goofday/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Goofday
+//
+//  Created by BAEKMAC on 4/14/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
